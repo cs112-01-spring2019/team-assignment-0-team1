79312ddac8abac525c6e06f9cc8b3ffd95a4629a
Kai-Eiji <kaiburkholder.jp@gmail.com>
2019-03-20 23:14:36 +0100

    DicePair

---
+++ DicePair.java
@@ -0,0 +1,48 @@
+import java.*;
+
+public class DicePair{
+	private Die dice1;
+	private Die dice2;
+
+	private int total;
+	
+
+	public DicePair()
+	{
+		dice1 = new Die();
+		dice2 = new Die();
+	}
+
+	public int Roll()
+	{
+		dice1.roll();
+		dice2.roll();
+		total = dice1.getFaceValue() + dice2.getFaceValue();
+		return total;
+	}
+
+	public Die getDice1()
+	{
+		return dice1;
+	}
+
+	public Die getDice2()
+	{
+		return dice2;
+	}
+
+	public int getTotal()
+	{
+		return total;
+	}
+
+
+	public String toString(){
+		String totalString = Integer.toString(total);
+		String stringNum1 = Integer.toString(dice1.getFaceValue());
+		String stringNum2 = Integer.toString(dice2.getFaceValue());
+		return "total number is " + stringNum1 + " + " + stringNum2 + " =" + totalString;
+	}
+
+
+}
